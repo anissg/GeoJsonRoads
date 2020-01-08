65baa26c4d687d34f11fb8d8820f251eefbbc9df
Anis <anis_sg@hotmail.com>
2020-01-08 16:10:35 +0100

    - first commit - geojson parser

---
+++ Assets/Scripts/GeoJsonRoadLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GeoJSON.Net;
+using GeoJSON.Net.Converters;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class GeoJsonRoadLoader : MonoBehaviour
+{
+    [SerializeField] private string resourceFileName;
+    private string _json;
+    private FeatureCollection _geoJsonFeatureCollection;
+    
+    void Start()
+    {
+        _json = Resources.Load<TextAsset>(resourceFileName)?.text;
+        _geoJsonFeatureCollection = JsonConvert.DeserializeObject<FeatureCollection>(_json, new GeoJsonConverter());
+
+        
+
+        //_geoJsonObject
+    }
+}
