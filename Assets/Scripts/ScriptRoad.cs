c4f787e29d14e3f258c9f40a1ebb71dcd736cda9
Romain Roy <rroy.romain@gmail.com>
2020-01-08 16:18:13 +0100

    Roads

---
+++ Assets/Scripts/ScriptRoad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PathCreator))]
+[RequireComponent(typeof(RoadCreator))]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class ScriptRoad : MonoBehaviour
+{
+    void Start()
+    {
+        Path path = GetComponent<PathCreator>().path;
+        path.points = new List<Vector3>
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(0, 0, 0),
+            new Vector3(0, 3, -1),
+            new Vector3(0, 3, -1),
+            new Vector3(0, 3, -1),
+            new Vector3(3, 3, 0),
+            new Vector3(3, 3, 0)
+        };
+    }
+
+    void Update()
+    {
+
+    }
+}
