1c3d0410f8bd74bdd1baf4bc9f67048c657bd6df
Anis HADJARI <anis_sg@hotmail.com>
2020-01-13 11:29:24 +0100

    - read roadcoordinates from geojson data

--- Assets/Scripts/GeoJsonRoadLoader.cs
+++ Assets/Scripts/GeoJsonRoadLoader.cs
@@ -20,7 +20,28 @@ public class GeoJsonRoadLoader : MonoBehaviour
         _geoJsonFeatureCollection = JsonConvert.DeserializeObject<FeatureCollection>(_json, new GeoJsonConverter());
 
         
+        
+    }
 
-        //_geoJsonObject
+    void DrawTroncon(Feature<MultiLineString,Dictionary<string,object>> feature)
+    {
+        MultiLineString itineraire = feature.Geometry;
+        string nom_itineraire = feature.Properties["NOM_ITI"].ToString();
+        double largeur_itineraire = (double) feature.Properties["LARGEUR"];
+        
+        GameObject path = new GameObject(nom_itineraire);
+        path.AddComponent<MeshRenderer>();
+        path.AddComponent<MeshFilter>();
+        path.AddComponent<RoadCreator>();
+        PathCreator pathCreator = path.AddComponent<PathCreator>();
+        pathCreator.path.points = new List<Vector3>();
+
+        foreach (LineString troncon in itineraire.Coordinates)
+        {
+            foreach (IPosition position in troncon.Coordinates)
+            {
+                pathCreator.path.points.Add(new Vector3((float)position.Longitude, (float)position.Altitude, (float)position.Latitude));        
+            }
+        }
     }
 }
